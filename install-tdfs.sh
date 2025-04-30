fc2126617209855ce097cecfaa4ca04acbef9162
Giovanni Bartolomeo <giobarty@gmail.com>
2025-04-30 16:10:55 +0200

    Update install-tdfs.sh

--- install-tdfs.sh
+++ install-tdfs.sh
@@ -31,9 +31,9 @@ if [ $? -ne 0 ]; then
 fi
 
 tar -xzf tdfs_${OS}_${ARCH}.tar.gz
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
     mv tdfs ~/bin/tdfs     
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
     sudo mv tdfs /usr/local/bin/tdfs
 fi
 
